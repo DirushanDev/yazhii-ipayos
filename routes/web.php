176dfeeb02d4365aa5b6e965dc7ed33643628a89
DirushanDev <rubakumardirushan@gmail.com>
2025-08-09 23:12:57 +0530

    remove wanted route

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Yazhii\Ipayos\Http\Controllers\IpayosController;
-Route::get('/ipayos/config', function () {
-    return response()->json([
-        'client_id' => config('ipayos.client_id'),
-        'token'     => substr(config('ipayos.token'), 0, 10) . '...',
-        'secret'    => substr(config('ipayos.secret'), 0, 10) . '...',
-        'endpoint'  => config('ipayos.apiendpoint'),
-    ]);
-});
 
 Route::get('/ipayos', function () {
     return view('ipayos::index'); // Use view namespace
